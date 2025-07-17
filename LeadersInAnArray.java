78faaebba36541365f92139bd05b819e48657013
asmahussain48 <asma.hussain.mughal@gmail.com>
2025-07-17 12:16:12 +0500

    initi commit

--- LeadersInAnArray.java
+++ LeadersInAnArray.java
@@ -1,3 +1,22 @@
-public class LeadersInAnArray {
+import java.util.ArrayList;
 
+public class LeadersInAnArray {
+    public static void main(String[] args) {
+
+        int[] Arr={16, 17, 4, 3, 5, 2};
+        int size=Arr.length;
+        int max=Arr[size - 1];
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        int[] result=new int[size];
+        for (int i=size-1; i >= 0; i--) {
+                if(Arr[i] <= max ){
+                    list.add(max);
+                }
+                max = Arr[i];
+
+        }
+        System.out.println(list);
+    }
 }
